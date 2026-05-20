3525b460d6781cc805fb2329b42b5c810fb4de49
Juliengaspar <julien.gaspar@student.hepl.be>
2026-05-20 09:12:23 +0200

    footer php && scss

--- wp-content/themes/PLAI/footer.php
+++ wp-content/themes/PLAI/footer.php
@@ -13,27 +13,39 @@ $copyrinthe = get_field('text__copyrinthe', 'option');
         <div class="footer__top">
             <div class="footer__img">
                 <?php
-
                 if($logo__img): ?>
-                    <img
-                            src="<?= esc_url($logo__img['url']); ?>"
-                            alt="<?= esc_attr($logo__img['alt']); ?>"
-                    >
+                    <img src="<?= esc_url($logo__img['url']); ?>" alt="<?= esc_attr($logo__img['alt']); ?>">
                 <?php endif; ?>
 
                 <?php if (!empty($social_media)) : ?>
                     <ul class="footer__socials" role="list">
                         <?php foreach ($social_media as $link) : ?>
                             <li class="footer__social-item">
-                                <a class="footer__social-link" href="<?= $link->href ?>" title="<?= $link->label ?>">
-                                    <?= $link->label ?>
-                                </a>
+                                <a class="footer__social-link" href="<?= $link->href ?>" title="<?= $link->label ?>"><?= $link->label ?></a>
                             </li>
                         <?php endforeach; ?>
                     </ul>
                 <?php endif; ?>
             </div>
+            <section class="partenaires">
+                <h2>PLAI</h2>
+                        <ul class="partenaires__liste">
+                <?php
+                $galerie = get_field('galerie__partenaires', 'option');
+                ?>
 
+                <?php if ($galerie) : ?>
+
+                    <?php foreach ($galerie as $image) : ?>
+                    <li>
+                        <img src="<?= esc_url($image['url']); ?>" alt="<?= esc_attr($image['alt']); ?>" class="partenaires__img">
+                    </li>
+                    <?php endforeach; ?>
+
+                <?php endif; ?>
+                        </ul>
+
+            </section>
             <nav class="footer__nav" aria-labelledby="footer-nav-title">
                 <h2 class="footer__title" id="footer-nav-title">Navigation</h2>
                 <ul class="footer__list" role="list">
